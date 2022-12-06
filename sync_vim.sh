c00cdc4154e03918cd40d3ec0fd062975f0c0092
Schuyler Manchester <smanchester@avantus.com>
2022-12-06 12:13:23 -0800

    Remove nvim stuff that I'm not currently using, and make tmux config sync executable.

--- sync_vim.sh
+++ sync_vim.sh
@@ -11,17 +11,17 @@ else
     printf "${MYVIM} not found. \n\n";
 fi
 
-if [ -f $MYNVIM ]; then
-    cp $MYNVIM ~/.config/nvim/init.vim;
-    printf "Successfully copied\n  \"${MYNVIM}\"  \n to \n  \"~/.config/nvim/init.vim\" \n\n";
-else
-    printf "${MYNVIM} not found. \n\n";
-fi
-
-if [ -f $MYCOCSETTINGS ]; then
-    cp $MYCOCSETTINGS ~/.config/nvim/coc-settings.json;
-    printf "Successfully copied\n  \"${MYCOCSETTINGS}\"  \n to \n  \"~/.config/nvim/coc-settings.json\" \n\n";
-else
-    printf "${MYCOCSETTINGS} not found. \n\n";
-fi
+# if [ -f $MYNVIM ]; then
+#     cp $MYNVIM ~/.config/nvim/init.vim;
+#     printf "Successfully copied\n  \"${MYNVIM}\"  \n to \n  \"~/.config/nvim/init.vim\" \n\n";
+# else
+#     printf "${MYNVIM} not found. \n\n";
+# fi
+# 
+# if [ -f $MYCOCSETTINGS ]; then
+#     cp $MYCOCSETTINGS ~/.config/nvim/coc-settings.json;
+#     printf "Successfully copied\n  \"${MYCOCSETTINGS}\"  \n to \n  \"~/.config/nvim/coc-settings.json\" \n\n";
+# else
+#     printf "${MYCOCSETTINGS} not found. \n\n";
+# fi
 
